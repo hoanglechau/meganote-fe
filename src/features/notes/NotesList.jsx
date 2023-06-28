f658e68e8020ae1966c95087c0f4065b2fd40458
Hoang Le Chau <44614317+hoanglechau@users.noreply.github.com>
2023-06-28 10:07:34 +0700

    Create NotesList.jsx

---
+++ src/features/notes/NotesList.jsx
@@ -0,0 +1,186 @@
+import {
+  Box,
+  Card,
+  Container,
+  Paper,
+  Stack,
+  Switch,
+  TablePagination,
+  Typography,
+} from "@mui/material";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import LoadingCircle from "../../components/LoadingCircle";
+import SearchInput from "../../components/SearchInput";
+import useAuth from "../../hooks/useAuth";
+import useTitle from "../../hooks/useTitle";
+import NoteTable from "./NoteTable";
+import { getNotes } from "./notesSlice";
+
+const NotesList = () => {
+  useTitle("Meganote: Notes List");
+
+  const { user } = useAuth();
+
+  const [filterName, setFilterName] = useState("");
+  const [filterCompleted, setFilterCompleted] = useState(false);
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(10);
+
+  const { isLoading, currentPageNotes, notesById, totalNotes } = useSelector(
+    state => state.notes
+  );
+  const notes = currentPageNotes.map(noteId => notesById[noteId]);
+  const dispatch = useDispatch();
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = event => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
+  const handleSubmit = searchQuery => {
+    setFilterName(searchQuery);
+  };
+
+  // Filter inactive notes
+
+  const handleFilterCompleted = () => {
+    setFilterCompleted(!filterCompleted);
+    setFilterName("");
+  };
+
+  useEffect(() => {
+    dispatch(
+      getNotes({
+        filterName,
+        filterCompleted,
+        page: page + 1,
+        limit: rowsPerPage,
+      })
+    );
+  }, [filterName, filterCompleted, page, rowsPerPage, dispatch]);
+
+  let filteredNotes = notes;
+  // Employees can only see their own notes
+  if (user.role === "Employee") {
+    filteredNotes = notes.filter(note => note.user === user._id);
+  }
+  // Managers cannot see Admins' notes, but can see all other notes
+  if (user.role === "Manager") {
+    filteredNotes = notes.filter(note => note.role !== "Admin");
+  }
+
+  let noteTable = null;
+  if (isLoading) {
+    noteTable = <LoadingCircle />;
+  } else {
+    noteTable = <NoteTable notes={filteredNotes} />;
+  }
+
+  return (
+    <Container
+      sx={{
+        p: { xs: 3, sm: 5, lg: 2.5 },
+        height: "100%",
+        width: "100%",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Paper
+        elevation={0}
+        sx={{
+          height: "100%",
+          width: "100%",
+        }}
+      >
+        <Typography
+          variant="h4"
+          color="primary"
+          sx={{
+            mt: { xs: 0, md: 0 },
+            mb: { xs: 1, md: 3 },
+            textAlign: "center",
+            fontSize: { xs: "1.5rem", sm: "2.5rem", lg: "2.5rem" },
+          }}
+        >
+          Notes List
+        </Typography>
+        <Card sx={{ p: { xs: 0.9, sm: 3, lg: 3 }, boxShadow: "none" }}>
+          <Stack spacing={2}>
+            <Stack direction={{ xs: "column", sm: "row" }} alignItems="center">
+              <Box
+                component="div"
+                sx={{
+                  display: "flex",
+                  justifyContents: "center",
+                  alignItems: "center",
+                }}
+              >
+                <SearchInput
+                  handleSubmit={handleSubmit}
+                  placeholder="Search notes..."
+                  sx={{ width: { xs: "100%", md: "300px" } }}
+                />
+
+                <Typography
+                  variant="subtitle"
+                  sx={{
+                    ml: 2,
+                    display: { xs: "none", md: "flex" },
+                  }}
+                >
+                  {totalNotes > 1
+                    ? `${totalNotes} notes found`
+                    : totalNotes === 1
+                    ? `${totalNotes} note found`
+                    : "No note found"}
+                </Typography>
+              </Box>
+
+              <Box sx={{ flexGrow: 1 }} />
+              <Box
+                component="div"
+                sx={{
+                  display: "flex",
+                  justifyContents: "flex-start",
+                  alignItems: "center",
+                }}
+              >
+                <Typography variant="subtitle">Hide completed</Typography>
+                <Switch
+                  checked={filterCompleted}
+                  onChange={handleFilterCompleted}
+                />
+              </Box>
+              <Box sx={{ flexGrow: 1 }} />
+              <TablePagination
+                sx={{
+                  "& .MuiTablePagination-selectLabel, .MuiTablePagination-select, .MuiTablePagination-selectIcon":
+                    {
+                      display: { xs: "none", md: "block" },
+                    },
+                }}
+                component="div"
+                count={totalNotes ? totalNotes : 0}
+                page={page}
+                onPageChange={handleChangePage}
+                rowsPerPage={rowsPerPage}
+                rowsPerPageOptions={[5, 10, 25]}
+                onRowsPerPageChange={handleChangeRowsPerPage}
+              />
+            </Stack>
+            {noteTable}
+          </Stack>
+        </Card>
+      </Paper>
+    </Container>
+  );
+};
+
+export default NotesList;
