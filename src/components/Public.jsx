17689c5090de9746ed2d7445012bd516d82fc089
Hoang Le Chau <44614317+hoanglechau@users.noreply.github.com>
2023-06-28 10:07:10 +0700

    Create Public.jsx

---
+++ src/components/Public.jsx
@@ -0,0 +1,61 @@
+import { Box, Divider, Paper, Typography } from "@mui/material";
+import ColorButton from "./ColorButton";
+
+const Public = () => {
+  const content = (
+    <Paper
+      component="section"
+      sx={{
+        height: "100vh",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Box
+        component="img"
+        sx={{
+          width: { xs: "17rem", sm: "34rem", lg: "50rem" },
+          height: { xs: "8.5rem", sm: "17rem", lg: "25rem" },
+          mb: { xs: "1rem", sm: "1.5rem", lg: "2.5rem" },
+          borderRadius: "8px",
+        }}
+        alt="Meganote Logo"
+        src="/images/Meganote-background.png"
+      />
+      <Divider />
+      <Box component="main">
+        <Typography
+          color="text"
+          sx={{ fontSize: { xs: "0.7rem", sm: "1.4rem", lg: "2rem" } }}
+        >
+          The ultimate task management experience for your team!
+        </Typography>
+      </Box>
+
+      <Box
+        component="footer"
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          gap: "1rem",
+          width: "fit-content",
+          mt: { xs: "1rem", sm: "2rem", lg: "3rem" },
+        }}
+      >
+        <ColorButton variant="contained" color="primary" to="/login">
+          User Login
+        </ColorButton>
+
+        <ColorButton variant="contained" color="primary" to="/register">
+          Register (For Demo)
+        </ColorButton>
+      </Box>
+    </Paper>
+  );
+
+  return content;
+};
+
+export default Public;
