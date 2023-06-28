2a4dcf8ff42db7dce986ec5a4ec752e875a59eb8
Hoang Le Chau <44614317+hoanglechau@users.noreply.github.com>
2023-06-28 10:08:09 +0700

    Create jwt.js

---
+++ src/utils/jwt.js
@@ -0,0 +1,19 @@
+import jwtDecode from "jwt-decode";
+
+export const isValidToken = accessToken => {
+  if (!accessToken) {
+    return false;
+  }
+  const decoded = jwtDecode(accessToken);
+  const currentTime = Date.now() / 1000;
+
+  return decoded.exp > currentTime;
+};
+
+export const decodeToken = accessToken => {
+  if (!accessToken) {
+    return null;
+  }
+  const decoded = jwtDecode(accessToken);
+  return decoded;
+};
