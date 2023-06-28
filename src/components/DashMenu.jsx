1e2ecc01b4b9deca3e058875ee72455f198bf099
Hoang Le Chau <44614317+hoanglechau@users.noreply.github.com>
2023-06-28 10:06:45 +0700

    Create DashMenu.jsx

---
+++ src/components/DashMenu.jsx
@@ -0,0 +1,35 @@
+import { Container, Paper } from "@mui/material";
+import Menu from "../features/auth/Menu";
+
+const DashMenu = () => {
+  return (
+    <Container
+      maxWidth="sm"
+      sx={{
+        minHeight: { xs: "87vh", sm: "87vh", lg: "87vh" },
+        maxHeight: { xs: "90vh", sm: "87vh", lg: "87vh" },
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        p: { xs: 3, sm: 5, lg: 2.5 },
+      }}
+    >
+      <Paper
+        elevation={1}
+        sx={{
+          p: { xs: 1, sm: 2, lg: 3 },
+          border: "1px solid #ccc",
+          boxShadow: "none",
+          width: "100%",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Menu />
+      </Paper>
+    </Container>
+  );
+};
+
+export default DashMenu;
