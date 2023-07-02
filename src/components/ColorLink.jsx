d38e4f88f755d011d0828c8c60035910156407ac
Hoang Le Chau <44614317+hoanglechau@users.noreply.github.com>
2023-07-03 02:12:04 +0700

    Update ColorLink.jsx

--- src/components/ColorLink.jsx
+++ src/components/ColorLink.jsx
@@ -1,6 +1,15 @@
 import { Typography, useTheme } from "@mui/material";
 import { Link } from "react-router-dom";
 
+/**
+ * @description A link component that works with react-router-dom
+ * @param {string} variant
+ * @param {string} color
+ * @param {string} to
+ * @param {*} children
+ * @param {object} other
+ * @author [Hoang Le Chau](https://github.com/hoanglechau)
+ */
 const ColorLink = ({ variant, to, color, children, ...other }) => {
   const theme = useTheme();
 
