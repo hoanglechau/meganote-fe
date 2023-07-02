8a64a85b7e322c945f917f2090f1dc79f96bf831
Hoang Le Chau <44614317+hoanglechau@users.noreply.github.com>
2023-07-03 02:13:51 +0700

    Update jwt.js

--- src/utils/jwt.js
+++ src/utils/jwt.js
@@ -1,5 +1,15 @@
 import jwtDecode from "jwt-decode";
 
+/**
+ * @description This file contains the functions to work with JWT
+ * @author [Hoang Le Chau](https://github.com/hoanglechau)
+ */
+
+/**
+ * @description Check if the token is valid
+ * @param {string} accessToken
+ * @returns {boolean} True if the token is valid, false otherwise
+ */
 export const isValidToken = accessToken => {
   if (!accessToken) {
     return false;
@@ -10,6 +20,11 @@ export const isValidToken = accessToken => {
   return decoded.exp > currentTime;
 };
 
+/**
+ * @description Decode the token
+ * @param {string} accessToken
+ * @returns {object} The decoded token
+ */
 export const decodeToken = accessToken => {
   if (!accessToken) {
     return null;
