f6b820c3688257f04801f8f6108f90cfb3341aa6
Hoang Le Chau <44614317+hoanglechau@users.noreply.github.com>
2023-07-03 02:12:00 +0700

    Update ColorButton.jsx

--- src/components/ColorButton.jsx
+++ src/components/ColorButton.jsx
@@ -1,6 +1,14 @@
 import { Button } from "@mui/material";
 import { Link } from "react-router-dom";
 
+/**
+ * @description A button component that works with react-router-dom
+ * @param {string} variant
+ * @param {string} color
+ * @param {string} to
+ * @param {*} children
+ * @author [Hoang Le Chau](https://github.com/hoanglechau)
+ */
 const ColorButton = ({ variant, color, to, children }) => {
   const linkStyle = {
     color: "#FFFFFF",
