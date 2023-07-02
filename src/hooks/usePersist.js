1a0e12c7f18aa5b26575da87379560ba00631ba3
Hoang Le Chau <44614317+hoanglechau@users.noreply.github.com>
2023-07-03 02:13:35 +0700

    Update usePersist.js

--- src/hooks/usePersist.js
+++ src/hooks/usePersist.js
@@ -3,6 +3,11 @@
 
 import { useState, useEffect } from "react";
 
+/**
+ * @description This hook is used to get the persist state from local storage
+ * @returns {Array} An array of persist state and a function to set persist state
+ * @author [Hoang Le Chau](https://github.com/hoanglechau)
+ */
 const usePersist = () => {
   // If 'persist' does not exist in our local storage, it will be 'false' initially
   const [persist, setPersist] = useState(
