32777662df84bd631176182de1723e118cc6a7f3
Hoang Le Chau <44614317+hoanglechau@users.noreply.github.com>
2023-07-03 02:13:33 +0700

    Update useAuth.js

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -1,6 +1,10 @@
 import { useContext } from "react";
 import { AuthContext } from "../contexts/AuthContext";
 
+/**
+ * @description This hook is used to get the auth context from AuthContextProvider
+ * @author [Hoang Le Chau](https://github.com/hoanglechau)
+ */
 const useAuth = () => {
   return useContext(AuthContext);
 };
