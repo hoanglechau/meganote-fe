331f2f4adaf31fb835dd03270cebee1196e057d4
Hoang Le Chau <44614317+hoanglechau@users.noreply.github.com>
2023-07-03 02:12:46 +0700

    Update statuses.js

--- src/config/statuses.js
+++ src/config/statuses.js
@@ -1,3 +1,8 @@
+/**
+ * @description This file contains the note statuses
+ * @author [Hoang Le Chau](https://github.com/hoanglechau)
+ */
+
 // Statuses for notes
 export const STATUSES = {
   Open: "Open",
