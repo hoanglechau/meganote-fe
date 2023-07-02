d7e90ee2a0b409424b41673f0ca0928fe08898b0
Hoang Le Chau <44614317+hoanglechau@users.noreply.github.com>
2023-07-03 02:12:13 +0700

    Update DashMenu.jsx

--- src/components/DashMenu.jsx
+++ src/components/DashMenu.jsx
@@ -1,6 +1,10 @@
 import { Container, Paper } from "@mui/material";
 import Menu from "../features/auth/Menu";
 
+/**
+ * @description The navigation menu for tablets and mobile devices
+ * @author [Hoang Le Chau](https://github.com/hoanglechau)
+ */
 const DashMenu = () => {
   return (
     <Container
@@ -17,7 +21,7 @@ const DashMenu = () => {
       <Paper
         elevation={1}
         sx={{
-          p: { xs: 1, sm: 2, lg: 3 },
+          p: { xs: 0, sm: 2, lg: 3 },
           border: "1px solid #ccc",
           boxShadow: "none",
           width: "100%",
