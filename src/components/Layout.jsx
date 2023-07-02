abffb50adc8222166d2846cd3d7e706ba3673b00
Hoang Le Chau <44614317+hoanglechau@users.noreply.github.com>
2023-07-03 02:12:31 +0700

    Update Layout.jsx

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,5 +1,9 @@
 import { Outlet } from "react-router-dom";
 
+/**
+ * @description The layout for the whole app
+ * @author [Hoang Le Chau](https://github.com/hoanglechau)
+ */
 const Layout = () => {
   return <Outlet />;
 };
