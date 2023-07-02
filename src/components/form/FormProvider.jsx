1a7ef50fc1ace73bc8d20e9530df0ad0f270cba2
Hoang Le Chau <44614317+hoanglechau@users.noreply.github.com>
2023-07-03 02:12:19 +0700

    Update FormProvider.jsx

--- src/components/form/FormProvider.jsx
+++ src/components/form/FormProvider.jsx
@@ -1,5 +1,12 @@
 import { FormProvider as RHFormProvider } from "react-hook-form";
 
+/**
+ * @description A wrapper component for react-hook-form
+ * @param {object} methods
+ * @param {function} onSubmit
+ * @param {*} children
+ * @author [Hoang Le Chau](https://github.com/hoanglechau)
+ */
 function FormProvider({ children, onSubmit, methods }) {
   return (
     <RHFormProvider {...methods}>
