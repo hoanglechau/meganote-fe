2b1fada66d16aa42438134e74db94b3a84b10b24
Hoang Le Chau <44614317+hoanglechau@users.noreply.github.com>
2023-07-03 02:11:58 +0700

    Update store.js

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { setupListeners } from "@reduxjs/toolkit/query";
 import notesReducer from "../features/notes/notesSlice";
 import themeReducer from "../features/theme/themeSlice";
 import usersReducer from "../features/users/usersSlice";
+
+/**
+ * @description This file is used to create a redux store for the whole app
+ * @author [Hoang Le Chau](https://github.com/hoanglechau)
+ */
 
 export const store = configureStore({
   // Configure the reducer, making it available to the whole app with the Provider in main.js
@@ -11,8 +15,6 @@ export const store = configureStore({
     users: usersReducer,
     notes: notesReducer,
   },
-  // Redux DevTools Extension
+  // Disable Redux DevTools Extension
   devTools: false,
 });
-
-setupListeners(store.dispatch);
