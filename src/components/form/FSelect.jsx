de489c965a207adccfae30c4559a147cf650776d
Hoang Le Chau <44614317+hoanglechau@users.noreply.github.com>
2023-07-03 02:12:23 +0700

    Update FSelect.jsx

--- src/components/form/FSelect.jsx
+++ src/components/form/FSelect.jsx
@@ -1,6 +1,13 @@
 import { TextField } from "@mui/material";
 import { Controller, useFormContext } from "react-hook-form";
 
+/**
+ * @description A select component that works with react-hook-form
+ * @param {string} name
+ * @param {*} children
+ * @param {object} other
+ * @author [Hoang Le Chau](https://github.com/hoanglechau)
+ */
 function FSelect({ name, children, ...other }) {
   const { control } = useFormContext();
 
