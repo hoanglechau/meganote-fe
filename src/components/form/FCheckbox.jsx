28d23c975e882c7d8691d8f1227b5829d3b77b8a
Hoang Le Chau <44614317+hoanglechau@users.noreply.github.com>
2023-07-03 02:12:15 +0700

    Update FCheckbox.jsx

--- src/components/form/FCheckbox.jsx
+++ src/components/form/FCheckbox.jsx
@@ -1,6 +1,12 @@
 import { Checkbox, FormControlLabel } from "@mui/material";
 import { Controller, useFormContext } from "react-hook-form";
 
+/**
+ * @description A checkbox component that works with react-hook-form
+ * @param {string} name
+ * @param {object} other
+ * @author [Hoang Le Chau](https://github.com/hoanglechau)
+ */
 function FCheckbox({ name, ...other }) {
   const { control } = useFormContext();
 
