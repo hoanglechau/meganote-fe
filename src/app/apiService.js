86fbe384925069cc0cb2b04875ba0293dfdf6799
Hoang Le Chau <44614317+hoanglechau@users.noreply.github.com>
2023-07-06 09:52:16 +0700

    Update apiService.js

--- src/app/apiService.js
+++ src/app/apiService.js
@@ -6,7 +6,7 @@ import axios from "axios";
  */
 
 const apiService = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000",
 });
 
 apiService.interceptors.request.use(
